c5a79a81b8414f19451d40347cf888b7775933de
unknown <Fasna-AM>
2024-11-30 13:22:55 +0530

    changes1

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.css'
+})
+export class EmployeeListComponent {
+
+  EmployeeForm :FormGroup
+
+  constructor(private fb:FormBuilder){
+    this.EmployeeForm = this.fb.group({
+      ename:['',[Validators.required,Validators.pattern("[a-zA-Z]*")]],
+      contactno:['',[Validators.required,Validators.pattern("[0-9]*")]],
+      email:['',[Validators.required,Validators.email]],
+      address:['',[Validators.required,Validators.pattern("[a-zA-Z0-9,]*")]],
+
+    })
+  }
+
+ 
+}
